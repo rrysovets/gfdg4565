dd1a4f0d1f1f9d19dd2966615534dc269150be95
rrysovets <rrysovets@gmail.com>
2025-04-28 18:44:23 +0300

    settings fix:)

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ environ.Env.read_env(BASE_DIR/'.env')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n7z-9j=vj)z$=^wq3pd$)wduuhuy)%_8w97i(m)$o^k*v@pwx@'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
